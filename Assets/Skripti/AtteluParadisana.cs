0adcd3a7471e692303f25f90c1baa97fabc21252
Narvils <gustavs.gn@gmail.com>
2022-02-23 14:09:54 +0200

    Importeti atteli

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
 	public Sprite[] Teli;
+	float vertiba1, vertiba2;
 	public GameObject mainigaisAttels;
+	public GameObject slaideris1, slaideris2;
 	public void izvele(int skaitlis){
 		if (skaitlis == 0) {
 			mainigaisAttels.GetComponent<Image> ().sprite = Teli [0];
@@ -14,4 +16,13 @@ public class AtteluParadisana : MonoBehaviour {
 		}
 			
 	}
+	//public void mainitPlatumu(){
+	//	vertiba1 = slaideris1.GetComponent<Slider> ().value;
+	//	mainigaisAttels.RectTransform.sizeDelta = new Vector2 (1F * vertiba1, vertiba2);
+	//}
+	//public void mainitGarumu(){
+	//	vertiba2 = slaideris2.GetComponent<Slider> ().value;
+	//	mainigaisAttels.RectTransform.sizeDelta = new Vector2 (vertiba1, 1F * vertiba2);
+	//}
+
 }
