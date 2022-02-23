f7c835e4ad6f1a52c2389c64f59aaec6e0fea2c1
Narvils <gustavs.gn@gmail.com>
2022-02-23 13:16:47 +0200

    Tela izvele

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+	public Sprite[] Teli;
+	public GameObject mainigaisAttels;
+	public void izvele(int skaitlis){
+		if (skaitlis == 0) {
+			mainigaisAttels.GetComponent<Image> ().sprite = Teli [0];
+		} else if (skaitlis == 1) {
+			mainigaisAttels.GetComponent<Image> ().sprite = Teli [1];
+		}
+			
+	}
+}
