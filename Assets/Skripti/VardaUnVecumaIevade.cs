765408f089a738105b1c526cdefe55baf6a532ec
Narvils <gustavs.gn@gmail.com>
2022-02-23 12:14:39 +0200

    Iesaktais projekts

---
+++ Assets/Skripti/VardaUnVecumaIevade.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaUnVecumaIevade : MonoBehaviour {	
+	public string vards;
+	public string vecums;
+	public GameObject ievadeslauksVards;
+	public GameObject ievadeslauksVecums;
+	public GameObject Izvada;
+	public void tekstaAttelosana(){
+		vards = ievadeslauksVards.GetComponent<Text> ().text;
+		vecums = ievadeslauksVecums.GetComponent<Text> ().text;
+		Izvada.GetComponent<Text>().text = "Sveiks mani sauc "+vards+", un man ir "+vecums+" gadi!";
+	}
+}
