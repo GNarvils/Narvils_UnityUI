08e2e7a7bb8a1bf7db2edbb68d307e9089a17645
Narvils <gustavs.gn@gmail.com>
2022-02-23 15:31:30 +0200

    Skana backup

---
+++ Assets/Skripti/KursorsUzAttela.cs
@@ -0,0 +1,15 @@
+﻿	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class KursorsUzAttela : MonoBehaviour {
+		public AudioSource skanasAvots;
+		public AudioClip skanaKoAtskenot;
+		public void  uzbiditsUzAttela(){
+		Debug.Log ("Skana Spele");
+		skanasAvots.PlayOneShot (skanaKoAtskenot);
+		}
+		public void  nobiditsUzAttela(){
+			skanasAvots.Stop ();
+		}
+	}
